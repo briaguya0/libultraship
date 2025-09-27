8a42025c728551f086266bcd2940f83fde65701b
briaguya <briaguya@pillar>
2025-09-27 06:22:28 -0400

    mac wminfo thing

--- src/utils/macUtils.mm
+++ src/utils/macUtils.mm
@@ -6,10 +6,13 @@
 
 //Just a simple function to toggle the native macOS fullscreen.
 void toggleNativeMacOSFullscreen(SDL_Window *window) {
-    SDL_SysWMinfo wmInfo;
-    SDL_VERSION(&wmInfo.version);
-    if (SDL_GetWindowWMInfo(window, &wmInfo)) {
-        NSWindow *nswindow = wmInfo.info.cocoa.window;
+    // https://wiki.libsdl.org/SDL3/README-migration#sdl_syswmh
+    // This header has been removed.
+    // 
+    // The information previously available in SDL_GetWindowWMInfo()
+    // is now available as window properties
+    NSWindow *nswindow = (__bridge NSWindow *)SDL_GetPointerProperty(SDL_GetWindowProperties(window), SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, NULL);
+    if (nswindow) {
         [nswindow toggleFullScreen:nil];
     }
 }
@@ -17,10 +20,13 @@ void toggleNativeMacOSFullscreen(SDL_Window *window) {
 //Just a simple function to check if we are in native macOS fullscreen mode. Needed to avoid the game from crashing
 //when going from native to SDL fullscreening modes or getting other forms of breakage.
 bool isNativeMacOSFullscreenActive(SDL_Window *window) {
-    SDL_SysWMinfo wmInfo;
-    SDL_VERSION(&wmInfo.version);
-    if (SDL_GetWindowWMInfo(window, &wmInfo)) {
-        NSWindow *nswindow = wmInfo.info.cocoa.window;
+    // https://wiki.libsdl.org/SDL3/README-migration#sdl_syswmh
+    // This header has been removed.
+    // 
+    // The information previously available in SDL_GetWindowWMInfo()
+    // is now available as window properties
+    NSWindow *nswindow = (__bridge NSWindow *)SDL_GetPointerProperty(SDL_GetWindowProperties(window), SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, NULL);
+    if (nswindow) {
         return (([nswindow styleMask] & NSWindowStyleMaskFullScreen) == NSWindowStyleMaskFullScreen);
     }
     return false;
